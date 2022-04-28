35deb53a53f8752c24fb89b9f90c8eff8d88c746
Ilham Bahari <muhari07@gmail.com>
2022-04-28 10:04:45 +0700

    refactor, testing

--- src/MqHttp.php
+++ src/MqHttp.php
@@ -5,19 +5,19 @@ namespace Qoligo\Package;
 use GuzzleHttp\Client as HttpClient;
 
 class MqHttp {
-    
+
     public function __construct()
     {
         $this->connection = new HttpClient();
     }
 
-    public function publishMessage($form_params, $config_base_url, $config_producer_url, $authorization)
+    public function publishMessage($form_params, $env)
     {
       try {
-        $this->connection->request('POST', $config_base_url . $config_producer_url, [
+        $this->connection->request('POST', $env['config_base_url'] . $env['config_producer_url'], [
             'form_params' => $form_params,
             \GuzzleHttp\RequestOptions::HEADERS => [
-                'Authorization' => $authorization
+                'Authorization' => $env['authorization']
             ]
         ]);
       } catch (\Exception $e) {
